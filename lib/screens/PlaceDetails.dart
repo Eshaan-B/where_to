7091f2069e7a4f88031e1989b9fc7eb4f561f66a
Reeti jha <70524989+Reeti1605@users.noreply.github.com>
2021-08-08 16:48:50 +0530

    place details page UI

--- lib/screens/PlaceDetails.dart
+++ lib/screens/PlaceDetails.dart
@@ -18,7 +18,79 @@ class _PlaceDetailsState extends State<PlaceDetails> {
     //place.userName
 
     return Container(
-      child: Text('${place.userName}'),
+      padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Text('${place.userName}`s Places',
+            style: TextStyle(fontSize: 28,
+                decoration: TextDecoration.none,
+            color: Colors.black),
+
+          ),
+          SizedBox(
+            height: 30,
+            width: 30
+          ),
+          Container(
+            color: Colors. lightBlue,
+            padding: EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+
+          ),
+          SizedBox(
+              height: 30,
+              width: 30
+          ),
+          Text('Description of ${place.locationName}',
+            style: TextStyle(fontSize: 28,
+                decoration: TextDecoration.none,
+                color: Colors.black),
+          ),
+          SizedBox(
+              height: 30,
+              width: 30
+          ),
+          Text('${place.description}',
+            style: TextStyle(fontSize: 18,
+                decoration: TextDecoration.none,
+                color: Colors.black),
+          ),
+          SizedBox(
+              height: 30,
+              width: 30
+          ),
+          Text('Images of ${place.locationName}',
+            style: TextStyle(fontSize: 28,
+                decoration: TextDecoration.none,
+                color: Colors.black),
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors. lightBlue,
+                margin: const EdgeInsets.all(20.0),
+                width: 160.0,
+                height: 140.0,
+
+              ),
+
+              Container(
+                color: Colors. lightBlue,
+                margin: const EdgeInsets.all(10.0),
+                width: 160.0,
+                height: 140.0,
+
+              ),
+            ],
+          )
+
+
+
+        ],
+      ),
+
+
     );
   }
 }
